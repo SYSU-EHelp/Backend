7a9615f1446187b9bfe316e46650fa788493464b
胡南 <1067861587@qq.com>
2017-05-09 23:54:37 +0800

    完成界面基本布局

---
+++ Android/EHelp/app/src/main/java/com/example/limin/ehelp/Information.java
@@ -0,0 +1,13 @@
+package com.example.limin.ehelp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Information extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information);
+    }
+}
